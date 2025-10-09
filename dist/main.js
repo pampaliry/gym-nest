c8428ea8a0ab5b3e6deb1ba82fb4b3fb4470fabd
pampaliry <matko.matus22@gmail.com>
2025-10-09 15:42:44 +0200

    opravy

--- dist/main.js
+++ dist/main.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const common_1 = require("@nestjs/common");
 const core_1 = require("@nestjs/core");
+require("dotenv/config");
 const app_module_1 = require("./app.module");
 const swagger_1 = require("@nestjs/swagger");
 const all_exceptions_filter_1 = require("./all-exceptions.filter");
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import 'dotenv/config';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AllExceptionsFilter } from './all-exceptions.filter';
